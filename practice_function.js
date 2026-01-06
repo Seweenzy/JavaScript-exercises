73b021079b27969cd833f4acee03fc688b5d13c5
Seweenzy <koluwaseunemmanuel@gmail.com>
2026-01-06 15:43:00 -0800

    Create practice_function.js

---
+++ practice_function.js
@@ -0,0 +1,55 @@
+function divideTwoNumbers(divide1, divide2){
+    let divideTotal= divide1 / divide2;
+    let multiplyTotal= divide1 * divide2;
+    let additionTotal= divide1 + divide2;
+    //console.log(divideTotal)
+    //console.log(multiplyTotal)
+    //console.log(additionTotal)
+
+}
+
+divideTwoNumbers(10,10)
+
+function collectThings(x,...y){
+    //console.log(x)
+    //console.log(y)
+}
+collectThings(1,2,3,4,5,6,7,8,9,10)
+
+
+number=100;
+for(i=1; i<=number; i++){
+    if(i % 2==0){
+        //console.log(i)
+    }
+}
+
+
+number=100;
+for(i=1; i<=number; i++){
+
+    if (i>66) {
+        break;
+     }
+
+    if(i % 2!==0){
+        //console.log(i)
+    }
+}
+
+
+var x=20;
+const y=30;
+let z=40;
+
+function doSomething(){
+    //console.log(x,y,z);
+}
+doSomething();
+
+
+fruitsArray=['banana','mango','cashew','guava','orange','cherry','watermelon','coconut']
+for(i=0; i<fruitsArray.length; i++){
+    console.log(fruitsArray[i]);
+
+}
